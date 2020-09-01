9e38d573207810971346fb92616ea82da5681e38
Himanshu Jain <himanshu14071999@gmail.com>
2020-09-01 17:04:23 +0530

    cors update

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'https://localhost:3443','http://localhost:3006','http://desktop-d6scll9:3006'];
+const whitelist = ['http://localhost:3000', 'https://localhost:3443','http://localhost:3006','https://restaurantclient.netlify.app'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
